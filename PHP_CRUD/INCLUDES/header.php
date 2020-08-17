1559710b9780dd8d812e8f0af6f443cf3d10bbc0
Nadia Irina Mendez <69281808+Nadia-Irina-Mendez@users.noreply.github.com>
2020-08-17 19:35:09 -0300

    Add files via upload

---
+++ PHP_CRUD/INCLUDES/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="widht-device-widht, initial-scale.1.0">
+  <meta http-equiv="x-UA-Compatible" content="ie-edge">
+  <title>PHP MySQL CRUD</title>
+  <!--BOOTSTRAP 4-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
